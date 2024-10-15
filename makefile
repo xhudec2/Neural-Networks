2e1464f5452a7d86eabed2bbe2bc079e0b30ff14
Marek <mareklicko448@gmail.com>
2024-10-15 23:22:13 +0200

    Added release mode (optimizations) to makefile and changed run.sh to do something

--- makefile
+++ makefile
@@ -1,5 +1,11 @@
 SRC = src/*.cpp
+
 FLAGS = -std=c++20 -Wall -Wextra -Werror -pedantic -g
+release: FLAGS += -O3 -funroll-loops -march=native 
+# -flto -fprefetch-loop-arrays -fno-rtti -ffast-math
 
 all:
 	g++ $(FLAGS) $(SRC) -o net
+
+release: all
+
--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ echo "#################"
 
 ## dont forget to use comiler optimizations (e.g. -O3 or -Ofast)
 # g++ -Wall -std=c++17 -O3 src/main.cpp src/file2.cpp -o network
+make release
 
 
 echo "#################"
@@ -21,4 +22,4 @@ echo "#################"
 ## use nice to decrease priority in order to comply with aisa rules
 ## https://www.fi.muni.cz/tech/unix/computation.html.en
 ## especially if you are using multiple cores
-# nice -n 19 ./network
+nice -n 19 ./net

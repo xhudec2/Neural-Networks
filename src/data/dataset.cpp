9f9e7da2e303c82c039752d20cc3d5909f5c1447
xhudec2 <xhudec2@Lukass-MacBook-Air.local>
2024-10-25 19:35:45 +0200

    add dataset functionality

---
+++ src/data/dataset.cpp
@@ -0,0 +1,6 @@
+#include "dataset.hpp"
+
+void Dataset::get_next_batch(size_t from, bool val, Matrix &batch) {
+    if (val) from += val_start;
+    std::memcpy(batch.data.data(), &data.data.data()[from * IMG_SIZE], batch_size * IMG_SIZE);
+}
---
+++ src/data/dataset.hpp
@@ -0,0 +1,28 @@
+#ifndef DATASET_HPP
+#define DATASET_HPP
+
+#include "../matrix/matrix.hpp"
+#include "../parser/csv_reader.hpp"
+#include <string>
+#include <utility>
+
+const size_t IMG_SIZE = 28*28;
+const size_t DATASET_SIZE = 60000;
+
+struct Dataset {
+    Matrix data;
+    size_t batch_size;
+    size_t val_start;
+    bool shuffle = false;
+
+    Dataset(std::string path, size_t batch_size, size_t train_size) : 
+            batch_size{batch_size}, val_start{train_size} {
+        std::vector<size_t> shape {DATASET_SIZE, IMG_SIZE};
+        CSV file(shape, path);
+        data = std::move(file.data);
+    }
+
+    void get_next_batch(size_t, bool, Matrix &);
+};
+
+#endif

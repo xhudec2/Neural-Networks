2953f1a1dbb350e9658f88b9882801cb6d2020ae
xhudec2 <xhudec2@Lukass-MacBook-Air.local>
2024-10-25 19:50:22 +0200

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include "matrix/tests.hpp"
+#include "parser/csv_reader.hpp"
+#include "matrix/printer.hpp"
+#include "data/dataset.hpp"
 
 int main() {
     tests();
+
+    std::string path = "/Users/xhudec2/Documents/School/5. semester/PV021/Neural-Networks/data/fashion_mnist_train_vectors.csv";
+
+    Dataset ds(path, 512, 53760);
+    size_t batch_size = 1024;
+    Matrix batch({batch_size, 28*28});
+
+    for (size_t epoch = 0; epoch < 100; ++epoch) {
+        std::cout << "epoch: " << epoch << '\n';
+        for (size_t x = 0; x < 53760 / batch_size; ++x) {
+            ds.get_next_batch(x * batch_size, false, batch);
+        }
+    }
+
+
+
     return 0;
 }

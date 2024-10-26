cb1a856b943f701cccc523543d7d03bfed69be70
xhudec2 <xhudec2@Lukass-MacBook-Air.local>
2024-10-26 20:52:29 +0200

    add main with network

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,24 @@
 #include "tests/tests.hpp"
 #include "parser/csv.hpp"
 #include "constants.hpp"
+#include "network/network.hpp"
 
 int main() {
-    matrix_tests();
-    csv_tests();
+    // matrix_tests();
+    // csv_tests();
 
-    Dataset ds(TRAIN_VEC_PATH, 512, 53760);
-    size_t batch_size = 1024;
-    Matrix batch({batch_size, 28 * 28});
+    // Dataset ds(TRAIN_VEC_PATH, 512, 53760);
+    // size_t batch_size = 1024;
+    // Matrix batch({batch_size, 28 * 28});
 
-    for (size_t epoch = 0; epoch < 100; ++epoch) {
-        std::cout << "epoch: " << epoch << '\n';
-        for (size_t x = 0; x < 53760 / batch_size; ++x) {
-            ds.get_next_batch(x * batch_size, false, batch);
-        }
-    }
+    // for (size_t epoch = 0; epoch < 100; ++epoch) {
+    //     std::cout << "epoch: " << epoch << '\n';
+    //     for (size_t x = 0; x < 53760 / batch_size; ++x) {
+    //         ds.get_next_batch(x * batch_size, false, batch);
+    //     }
+    // }
+    Network net({2, 5, 3, 1});
+    net.train();
 
     return 0;
 }
